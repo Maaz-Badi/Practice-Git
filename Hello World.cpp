2cb75adb61690f270305f7b64643f254a8e3bbe7
Maaz-Haris <maazharis3210@outlook.com>
2025-08-01 22:20:48 +0500

    made this code for marks

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-#include <chrono>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    auto start = chrono::high_resolution_clock::now();
-
-    for (int i = 1; i <= 1000000; ++i) {
-        cout << i << '\n';  // Fast: no flush
+int main() 
+{
+    int mark;
+    cout << "Enter your mark: "; 
+    cin >> mark;
+    if (mark >= 50) {
+        cout << "Pass" << endl;
+    } else {
+        cout << "Fail" << endl;
     }
-
-    auto end = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> exec_time = end - start;
-
-    cout << "Execution time: " << exec_time.count() << " ms\n";
-
-    return 0;
 }
