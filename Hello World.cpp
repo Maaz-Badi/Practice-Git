abe8eec17b9b8af67639decd12123c830b5ceb08
Maaz-Haris <maazharis3210@outlook.com>
2025-08-08 23:37:00 +0500

    learned more about C++

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
 
+double circle_area_calculator(double radius) 
+{
+    const double pi = 3.14159;
+    double area = pi * radius* radius;
+    return area;
+}
+
 int main() 
 {
-    int i = 0;
-    while (i <=1000) {
-        if  (i%2 == 0) {
-            cout << i << "\n";
-        }
-        i++;
-    }
+    double radius;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    double area = circle_area_calculator(radius);
+    cout << "The area of the circle with radius " << radius << " is: " << area << "cm^2 \n";
+
+    return 0;
+
 }
--- import time.py
+++ import time.py
@@ -1,10 +1,3 @@
-import time
-
-start_time = time.time()
-
-for i in range(1, 10001):
-    print(i)
-7124
-end_time = time.time()
-execution_time = end_time - start_time
-print(f"Execution time: {execution_time * 1000:.3f} ms")
+table = int(input("Enter the Table: "))
+for i in range (1, 11):
+    print(f"{table} x {i} = {table * i}")
